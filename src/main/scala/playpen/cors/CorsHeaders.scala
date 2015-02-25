9a6f3c6f91248946e11b7521ba5ec21c112072d9
Vincent Munier <vmunier.fr@gmail.com>
2015-02-25 11:42:24 +0000

    Prefix config keys with playpen.

--- src/main/scala/playpen/cors/CorsHeaders.scala
+++ src/main/scala/playpen/cors/CorsHeaders.scala
@@ -3,8 +3,8 @@ import play.api.Play._
 import play.api.mvc.RequestHeader
 
 object CorsHeaders {
-  lazy val allowedOrigins = current.configuration.getStringSeq("cors.allowed.origins").getOrElse(Seq())
-  lazy val allowedMethods = current.configuration.getStringSeq("cors.allowed.methods").getOrElse(Seq("POST", "GET", "OPTIONS", "PUT", "DELETE"))
+  lazy val allowedOrigins = current.configuration.getStringSeq("playpen.cors.allowed.origins").getOrElse(Seq())
+  lazy val allowedMethods = current.configuration.getStringSeq("playpen.cors.allowed.methods").getOrElse(Seq("POST", "GET", "OPTIONS", "PUT", "DELETE"))
 
   def headers(request: RequestHeader): Seq[(String, String)] = request match {
     case CorsParams(params) if allowedOrigins.isEmpty | allowedOrigins.exists(params.origin.endsWith(_)) =>
